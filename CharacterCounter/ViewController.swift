b9e67797643ed7531bf0ef667a335a89678e3a0f
Admin <admin@mac-admin.mshome.net>
2016-09-14 17:15:51 +0300

    v1 son

--- CharacterCounter/ViewController.swift
+++ CharacterCounter/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet var textView: UITextView!
 
+    @IBOutlet var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        textView.layer.cornerRadius = 5.0;
+        textView.layer.masksToBounds = false;
+        textView.layer.borderWidth = 0.5;
+        textView.layer.shadowColor = UIColor.orange.cgColor;
+        textView.layer.shadowOpacity = 0.4;
+        textView.layer.shadowRadius = 5.0;
 
+        textView.delegate = self
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn  range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            performAction()
+            return false
+        }
+        return true
+    }
+    
+    func performAction() {
+        var sayac = textView.text.characters.count;
+        lbl.text = String(sayac);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
